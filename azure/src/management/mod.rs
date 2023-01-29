02760faf90a701a2782b6f99876021642122a564
Arnavion <me@arnavion.dev>
2023-01-29 01:08:30 -0800

    Drop once_cell2 crate since it's only used in one place now.

--- azure/src/management/mod.rs
+++ azure/src/management/mod.rs
@@ -38,12 +38,11 @@ impl crate::Client for Client<'_> {
 	const AUTH_RESOURCE: &'static str = "https://management.azure.com";
 
 	fn make_url(&self, path_and_query: std::fmt::Arguments<'_>) -> anyhow::Result<http::uri::Parts> {
-		static AUTHORITY: once_cell2::race::LazyBox<http::uri::Authority> =
-			once_cell2::race::LazyBox::new(|| http::uri::Authority::from_static("management.azure.com"));
+		static AUTHORITY: once_cell::race::OnceBox<http::uri::Authority> = once_cell::race::OnceBox::new();
 
 		let mut url: http::uri::Parts = Default::default();
 		url.scheme = Some(http::uri::Scheme::HTTPS);
-		url.authority = Some(AUTHORITY.clone());
+		url.authority = Some(AUTHORITY.get_or_init(|| Box::new(http::uri::Authority::from_static("management.azure.com"))).clone());
 		url.path_and_query = Some(
 			format!("/subscriptions/{}/resourceGroups/{}{path_and_query}", self.subscription_id, self.resource_group_name)
 			.try_into().context("could not parse request URL")?,
